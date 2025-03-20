8c8d1d623c204b3f034eea6619ef512cf39e443b
Lorenzo Bordignon <lorenzobordignon73@gmail.com>
2025-03-20 14:14:19 -0300

    feat: finalizando curso 3 JavaScript

--- Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 3 - JavaScript/app/metodoFilter.js
+++ Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 3 - JavaScript/app/metodoFilter.js
@@ -3,8 +3,26 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros)) //para cada 
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id) //this - refere-se ao elemento que disparou o evento(btn)
-    const categoria = elementoBtn.value //oi oi dnv
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
 
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponivel.innerHTML = `<div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>`
 }
