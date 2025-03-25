f72d72b5a07afa2879c503309fcec509ed9d0105
Lorenzo Bordignon <lorenzobordignon73@gmail.com>
2025-03-25 11:49:24 -0300

    inicio curso 4 JavaScript

---
+++ Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 4 - JavaScript/script.js
@@ -0,0 +1,56 @@
+const html = document.querySelector('html')
+const botaoFoco = document.querySelector('.app__card-button--foco')
+const botaoDescansoCurto = document.querySelector('.app__card-button--curto')
+const botaoDescansoLongo = document.querySelector('.app__card-button--longo')
+const imagemPrincipal = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const inputMusicaFoco = document.getElementById('alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+inputMusicaFoco.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
+botaoFoco.addEventListener('click', () => {
+    alterarContexto('foco')
+    botaoFoco.classList.add('active')
+})
+
+botaoDescansoCurto.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+    botaoDescansoCurto.classList.add('active')
+})
+
+botaoDescansoLongo.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+    botaoDescansoLongo.classList.add('active')
+})
+
+function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)
+    imagemPrincipal.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+        default:
+            break;
+    }
+}
