84847e554fa50d1cde8fd4580c86c989a3c142ec
Lorenzo Bordignon <lorenzobordignon73@gmail.com>
2025-04-04 09:04:12 -0300

    feat: progresso curso 6

--- Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 6 - JavaScript/js/api.js
+++ Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 6 - JavaScript/js/api.js
@@ -3,8 +3,23 @@ const api = {
         try {
             const response = await fetch('http://localhost:3000/pensamentos')
             return await response.json()
-        } catch {
+        } catch (error) {
             alert('Erro ao buscar pensamentos.')
+            throw error
+        }
+    },
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } catch {
+            alert('Erro ao salvar pensamento.')
             throw error
         }
     }
--- Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 6 - JavaScript/js/main.js
+++ Formacao - Desenvolvimento FrontEnd cursos para criar aplicações web com HTML, CSS e JavaScript/Curso 6 - JavaScript/js/main.js
@@ -1,5 +1,35 @@
 import ui from './ui.js'
+import api from './api.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById('pensamento-form')
+    const botaoCancelarForm = document.getElementById('botao-cancelar')
+    formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario)
+    botaoCancelarForm.addEventListener("click", manipularCancelamento)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+    const id = document.getElementById('pensamento-id').value
+    const conteudo = document.getElementById('pensamento-conteudo').value
+    const autoria = document.getElementById('pensamento-autoria').value
+
+    try {
+        await api.salvarPensamento({
+            conteudo,
+            autoria
+        })
+        ui.renderizarPensamentos()
+        ui.limparCamposForm()
+    } catch (error) {
+        limparCamposForm()
+        alert('Erro ao salvar pensamento')
+        throw error
+    }
+}
+
+function manipularCancelamento() {
+    ui.limparCamposForm()
+}
