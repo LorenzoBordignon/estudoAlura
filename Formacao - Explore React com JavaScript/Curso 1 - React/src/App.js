4bd1b8b42e25aae477c797905287aec276843c03
Lorenzo Bordignon <lorenzobordignon73@gmail.com>
2025-05-08 13:32:30 -0300

    feat: progresso curso 1 de React

--- Formacao - Explore React com JavaScript/Curso 1 - React/src/App.js
+++ Formacao - Explore React com JavaScript/Curso 1 - React/src/App.js
@@ -1,11 +1,11 @@
 import Banner from "./componentes/Banner/Banner.js";
-import CampoTexto from "./componentes/CampoTexto";
+import Formulario from "./componentes/Formulario/index.js";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto />
+      <Formulario />
     </div>
   );
 }
