e7fc2ec07a1b0028d904487823498491b28aa028
Lorenzo Bordignon <lorenzobordignon73@gmail.com>
2025-05-14 13:09:16 -0300

    feat: progresso curso 1 react

--- Formacao - Explore React com JavaScript/Curso 1 - React/src/componentes/CampoTexto/index.js
+++ Formacao - Explore React com JavaScript/Curso 1 - React/src/componentes/CampoTexto/index.js
@@ -1,10 +1,21 @@
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const placeholderModificada = `${props.placeholder}...`;
+
+  const aoDigitado = (event) => {
+    props.aoAlterado(event.target.value);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder} />
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
     </div>
   );
 };
