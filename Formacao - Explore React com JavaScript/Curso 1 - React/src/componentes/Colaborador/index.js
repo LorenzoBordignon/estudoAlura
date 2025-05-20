99edb50db823f77d4a6e7ffd511d9080576184b5
Lorenzo Bordignon <lorenzobordignon73@gmail.com>
2025-05-20 08:25:10 -0300

    feat: Concluindo Curso 1 ReactJs

--- Formacao - Explore React com JavaScript/Curso 1 - React/src/componentes/Colaborador/index.js
+++ Formacao - Explore React com JavaScript/Curso 1 - React/src/componentes/Colaborador/index.js
@@ -6,7 +6,7 @@ const Colaborador = ({ nome, cargo, imagem, corDeFundo }) => {
       <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
         <img src={imagem} alt={nome} />
       </div>
-      <div className="rodape">
+      <div className="rodapeColaborador">
         <h4>{nome}</h4>
         <h5>{cargo}</h5>
       </div>
